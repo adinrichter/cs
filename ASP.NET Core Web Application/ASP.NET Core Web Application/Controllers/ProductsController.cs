507878ac1dee6b0ac58a315737f50d398c30c4b0
adinrichter <47506271+adinrichter@users.noreply.github.com>
2022-12-07 12:21:04 -0800

    Moved .net core web app here

---
+++ ASP.NET Core Web Application/ASP.NET Core Web Application/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Web_Application.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
